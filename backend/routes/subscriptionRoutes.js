ccce123f4e6b5e35fe90bcc6cc1fa2dca7d9a88e
NS <Nigus.Solomon.Dev@outlook.com>
2026-08-01 23:47:12 +0300

    feat: add PUT /upgrade route for plan changes

--- backend/routes/subscriptionRoutes.js
+++ backend/routes/subscriptionRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createSubscription, cancelSubscription, getMySubscription, createPortalSession } = require('../controllers/subscriptionController');
+const { createSubscription, cancelSubscription, getMySubscription, createPortalSession, upgradeSubscription,  } = require('../controllers/subscriptionController');
 const { protect } = require('../middleware/auth');
 
 router.post('/create', protect, createSubscription);
 router.delete('/cancel', protect, cancelSubscription);
 router.get('/me', protect, getMySubscription);
 router.post('/portal', protect, createPortalSession);
-
+router.put('/upgrade', protect, upgradeSubscription);
 
 module.exports = router;
