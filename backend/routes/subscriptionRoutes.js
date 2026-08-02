15f959f4f2d707daf0680ca5e608700da840271f
NS <Nigus.Solomon.Dev@outlook.com>
2026-08-02 17:07:43 +0300

    fix: expose subscription me endpoint and billing routes

--- backend/routes/subscriptionRoutes.js
+++ backend/routes/subscriptionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createSubscription, cancelSubscription, getMySubscription, createPortalSession, upgradeSubscription,  } = require('../controllers/subscriptionController');
+const { createSubscription, cancelSubscription, getMySubscription, createPortalSession, upgradeSubscription, resumeSubscription } = require('../controllers/subscriptionController');
 const { protect } = require('../middleware/auth');
 
 router.post('/create', protect, createSubscription);
@@ -8,5 +8,6 @@ router.delete('/cancel', protect, cancelSubscription);
 router.get('/me', protect, getMySubscription);
 router.post('/portal', protect, createPortalSession);
 router.put('/upgrade', protect, upgradeSubscription);
+router.put('/resume', protect, resumeSubscription);
 
 module.exports = router;
