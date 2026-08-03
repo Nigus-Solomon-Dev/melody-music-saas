f265b3d9255b8910f6f8c31327af23dde9de17ef
NS <Nigus.Solomon.Dev@outlook.com>
2026-08-03 22:37:48 +0300

    feat: add curated artists and album API routes

--- backend/routes/musicRoutes.js
+++ backend/routes/musicRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { search, track, fullPlayback, artist, lyrics, recommendations, chart } = require('../controllers/musicController');
+const { search, track, fullPlayback, artist, album, curatedArtists, lyrics, recommendations, chart } = require('../controllers/musicController');
 const { protect } = require('../middleware/auth');
+const { requirePlan } = require('../middleware/requirePlan');
 
 router.get('/search', search);
 router.get('/chart', chart);
 router.get('/track/:id', track);
-router.get('/track/:id/full', fullPlayback);
+router.get('/track/:id/full', protect, requirePlan('pro'), fullPlayback);
 router.get('/artist/:id', artist);
-router.get('/track/:id/lyrics', lyrics);
-router.get('/recommendations', protect, recommendations);
+router.get('/album/:id', album);
+router.get('/artists/curated', curatedArtists);
+router.get('/track/:id/lyrics', protect, requirePlan('enterprise'), lyrics);
+router.get('/recommendations', protect, requirePlan('pro'), recommendations);
 
 module.exports = router;
