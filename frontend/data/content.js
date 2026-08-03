a97edd9f5824c3fc34d43aa1efeabd1191abc17b
NS <Nigus.Solomon.Dev@outlook.com>
2026-08-03 22:42:28 +0300

    feat: add static content data

---
+++ frontend/data/content.js
@@ -0,0 +1,19 @@
+export const trendingSongs = [
+  { img: 'https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=300', title: 'Animal', sub: 'KATSEYE' },
+  { img: 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300', title: 'Midnight', sub: 'Taylor Swift' },
+  { img: 'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300', title: 'Blinding Lights', sub: 'The Weeknd' },
+  { img: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=300', title: 'Stay', sub: 'Justin Bieber' },
+];
+
+export const popularAlbums = [
+  { img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=300', title: 'Ende Kal', sub: 'Eyob Mekonen' },
+  { img: 'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?w=300', title: 'Rendezvous', sub: 'Hewan G.' },
+  { img: 'https://images.unsplash.com/photo-1487180144351-b8472da7d491?w=300', title: 'Selam', sub: 'Dibekulu' },
+  { img: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=300', title: 'Lij', sub: 'Zeritu Kebede' },
+];
+
+export const popularRadio = [
+  { bg: '#ff6450', color: 'white', img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=150', title: 'Yohana', desc: 'With Dibekulu, Hewan Gebrewold...' },
+  { bg: '#83e1ad', color: 'black', img: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=150', title: 'leul sisay', desc: 'With Mesay Tefera, Mastewal Eyayu...' },
+  { bg: '#ffe66d', color: 'black', img: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=150', title: 'Aster Aweke', desc: 'With Tilahun, Mahmoud Ahmed...' },
+];
